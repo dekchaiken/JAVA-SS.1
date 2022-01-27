0d1af6cda0e64291fc55d2cc2430b4cae808145f
Kennoi <644607030007@dpu.ac.th>
2022-01-27 14:06:43 +0700

    Week2
    Work1

---
+++ Week2/Work2_2.java
@@ -0,0 +1,8 @@
+public class Work2_2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
