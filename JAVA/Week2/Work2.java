64896baa1bbe42498a1e3fca644156f14fc87a1b
Kennoi <644607030007@dpu.ac.th>
2022-01-27 14:02:46 +0700

    Week1

---
+++ JAVA/Week2/Work2.java
@@ -0,0 +1,9 @@
+public class Work2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
---
+++ JAVA/Week2/Work7.java
@@ -0,0 +1,13 @@
+public class Work7 {
+    public static void main(String[] strings) {
+
+        double kilometers = 14.0;
+        double miles = kilometers / 1.6;
+
+        double time = (45.5 * 60.0 + 30.0) / (60.0 * 60.0);
+        double milesPerHour = miles / time;
+
+        System.out.println(milesPerHour);
+
+    }
+}
